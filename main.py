7b0f448c0f45d4bb42bbe6e22e839bf2352d1901
Eoin Wickens <ewickens@EW-TR-C02VT2GYHV2H.local>
2019-02-21 12:31:23 +0000

    First commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+import sys
+import scandir
+import pefile
+
+# code for scanning files in directory
+# if os.path.isdir(args.file):
+#         for root, dirs, files in scandir.walk(args.file):
+#             for file in files:
+
