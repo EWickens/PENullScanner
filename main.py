7b62f71afffca0d39e4f3959700b4b105f6ff95a
ewickens <eoinwickens@gmail.com>
2019-02-22 12:31:00 +0000

    Added default buffer size and extra functionality

--- main.py
+++ main.py
@@ -13,58 +13,29 @@ import binascii
 #             for file in files:
 
 def main():
+    DEFAULT_BUFFER_SIZE = 800
+
     args = parse_arguments()  # Parses args for arguments
 
-    pe = pefile.PE(args.filename)  # Takes the filename from command line argument and loads PE file
+    file = args.filename
 
+    if args.buffer > 0:
+        buffer_size = args.buffer
+    else:
+        buffer_size = DEFAULT_BUFFER_SIZE
+
+    pe = pefile.PE(file)  # Takes the filename from command line argument and loads PE file
     num_sections = pe.FILE_HEADER.NumberOfSections
 
-    print("Number of Sections: " + str(num_sections))
+    verbose = False
+
+    if args.verbose:
+        verbose = True
 
     last_section = pe.sections[num_sections - 1]  # gets the last section
-    print("Writeable: " + str(check_writeable(last_section)))
-    print("Executable: " + str(check_executable(last_section)))
+    result = check_for_null(file, last_section, verbose, buffer_size)
 
-    print(last_section)
-
-    if (check_writeable(last_section) and check_executable(
-            last_section)):  # Checks to see if last section is writeable and executable
-
-        '''If the section size is in effective Twos Compliment (i.e. 0xFFFFXXXX) then adjust size accordingly'''
-        if "0xf" in hex(last_section.SizeOfRawData):
-            print("ENTERS")
-            print(hex(last_section.SizeOfRawData))
-            size = 0xFFFFFFFF - last_section.SizeOfRawData
-        else:
-            print(hex(last_section.SizeOfRawData))
-            size = last_section.SizeOfRawData
-
-        # Reads from the hex offset
-        split_hex = read_from_hex_offset(args.filename, hex(last_section.PointerToRawData), size)
-
-        # Converts into a list
-        split_hex = list(split_hex)
-
-        # print(split_hex)
-        contiguous_count = 0
-        highest_occurrences = 0
-
-        for val in split_hex:
-            if val == '\x00':
-                contiguous_count += 1
-
-                if contiguous_count > highest_occurrences:
-                    highest_occurrences = contiguous_count
-
-            else:
-                contiguous_count = 0
-
-        print("Highest Occurence: " + str(highest_occurrences))
-        if highest_occurrences > 300:
-            print("Lot of zero's there boss")
-
-    else:
-        exit()
+    print("Result: " + str(result))
 
 
 def check_executable(last_section):
@@ -84,18 +55,19 @@ def check_writeable(last_section):
 
 
 def parse_arguments():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        description="Parse a file for a RWX last section and determine if it contains a section of NULL bytes of a specified size")
 
     parser.add_argument("-f", "--file", dest="filename",
-                        help="Specify file to be scanned", metavar="FILE")
+                        help="Specify file to be scanned", metavar="<file>", required=True)
     parser.add_argument("-b", "--buffer", dest="buffer",
-                        help="Specifies how many 0's to look for default is ",
-                        metavar="BUFFER")
+                        help="Specifies how many 0's to look for default is - Default is 800 bytes",
+                        metavar="<buffSize>")
+    parser.add_argument("-v", "--verbose",
+                        help="Displays information regarding the PE File", action='store_true')
 
-
-    # TODO - DETERMINE DEFAULT NUMBER OF ZEROS & SET DEFAULT NUMBER
-    # TODO ADD HELP TEXT
     args = parser.parse_args()
+
 
     return args
 
@@ -111,6 +83,51 @@ def read_from_hex_offset(file, hex_offset, size):
         outfile.write(data)
 
     return data
+
+
+def check_for_null(file, last_section, verbose, buffer_size):
+    writeable = check_writeable(last_section)
+    executable = check_writeable(last_section)
+
+    if verbose:
+        print(last_section)
+        print("Writeable: " + str(writeable))
+        print("Executable: " + str(executable))
+
+    if writeable and executable:  # Checks to see if last section is writeable and executable
+
+        '''If the section size is in effective Twos Compliment (i.e. 0xFFFFXXXX) then adjust size accordingly'''
+        if "0xf" in hex(last_section.SizeOfRawData):
+            size = 0xFFFFFFFF - last_section.SizeOfRawData
+        else:
+            size = last_section.SizeOfRawData
+
+        # Reads from the hex offset
+        split_hex = read_from_hex_offset(file, hex(last_section.PointerToRawData), size)
+
+        # Converts into a list
+        split_hex = list(split_hex)
+
+        # print(split_hex)
+        contiguous_count = 0
+        highest_occurrences = 0
+
+        for val in split_hex:
+            if val == '\x00':
+                contiguous_count += 1
+
+                if contiguous_count > highest_occurrences:
+                    highest_occurrences = contiguous_count
+
+            else:
+                contiguous_count = 0
+
+        print("Highest Occurence: " + str(highest_occurrences))
+        if highest_occurrences > buffer_size:
+            return True
+
+    else:
+        return False
 
 
 main()
